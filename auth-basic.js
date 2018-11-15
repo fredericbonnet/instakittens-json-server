204c33b4dca4949622b46ef1479541e1df41e11e
Frederic Bonnet <fredericbonnet@free.fr>
2018-11-15 21:28:54 +0100

    Added Basic authentication middleware with the generated accounts database

---
+++ auth-basic.js
@@ -0,0 +1,46 @@
+/*
+ * Simple Basic authentication middleware.
+ *
+ * Simply checks that the username/password are in accounts.json file. The
+ * username is case insensitive.
+ */
+
+const base64 = require('base-64');
+
+/** Accounts database */
+const accounts = require('./accounts.json');
+
+/**
+ * Express middleware function.
+ */
+module.exports = function(req, res, next) {
+  try {
+    const authorization = req.get('Authorization');
+    if (!authorization) throw 'unauthenticated';
+
+    const [scheme, identity] = authorization.split(' ');
+    if (!scheme || !identity || scheme.toLowerCase() !== 'basic')
+      throw 'bad_scheme';
+
+    const [, username, password] = base64
+      .decode(identity)
+      .match(/([^:]+):(.*)/);
+    const account = accounts.find(
+      account => account.username.toLowerCase() === username.toLowerCase()
+    );
+    if (!account || account.password !== password) throw 'authentication_error';
+
+    // Remember auth info for further processing.
+    res.locals.auth = { ...account };
+
+    return next();
+  } catch (e) {
+    return res
+      .append(
+        'WWW-Authenticate',
+        'Basic realm="JSON-Server test"',
+        'charset="UTF-8"'
+      )
+      .sendStatus(401);
+  }
+};
