30defde4d320aa5006d13207f0221c82a30f71a9
Frederic Bonnet <fredericbonnet@free.fr>
2019-07-03 18:20:29 +0200

    Added /auth route for client apps

--- routes.js
+++ routes.js
@@ -1,8 +1,22 @@
 /*
- * This modules is used to apply custom routes at the end of the middleware chain. 
+ * This modules is used to apply custom routes at the end of the middleware chain.
  * By default json-server applies route rewriting rules before the other middlewares.
  * This can cause problems for e.g. access control middlewares.
  */
 const jsonServer = require('json-server');
+const express = require('express');
 
-module.exports = jsonServer.rewriter(require('./routes.json'));
+/**
+ * Express router for /auth route, returns the auth info.
+ */
+const router = express.Router();
+router
+  .route(['/auth'])
+  // Return auth info.
+  .get((req, res, next) => {
+    res.json(res.locals.auth);
+  })
+  // Reject everything else.
+  .all((req, res, next) => res.sendStatus(405));
+
+module.exports = [router, jsonServer.rewriter(require('./routes.json'))];
